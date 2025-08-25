5cf24851c0b24693ac2511ae901fb7c1349eda68
aathif satthar basha <aathifxx7@gmail.com>
2025-08-25 17:27:56 +0530

    elecronjs

---
+++ electron.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // Load React app
+  win.loadURL('http://localhost:5173');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
